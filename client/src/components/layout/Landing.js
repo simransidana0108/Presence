00afc3e5ddad1abb82eb6ad18c8f7a010f5e908a
simransidana0108 <csecec.1702884@gmail.com>
2020-05-24 19:00:07 +0530

    pushed project

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div
+          className="row"
+          style={{ background: "grey", height: "100px", display: "flex" }}
+        >
+          <div className="col s12 center-align" style={{ alignSelf: "center" }}>
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                }}
+                className="btn btn-large btn-flat waves-effect white black-text"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
